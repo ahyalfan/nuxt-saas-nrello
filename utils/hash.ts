499b94df87a5a50bb8ecaed6fd3f8e7f6ca9422e
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-08 22:19:34 +0700

    fix: hash

--- utils/hash.ts
+++ utils/hash.ts
@@ -3,6 +3,5 @@ import bcrypt from "bcryptjs";
 export async function generateHash(data: string, saltRound: number = 10)  {
   const salt = await bcrypt.genSalt(saltRound);
   const hashedData = await bcrypt.hash(data, salt);
-
   return hashedData;
 }
