18bf221a2c5565ae58e773ae336b689a0e67ba41
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-10 21:49:35 +0700

    feat: realtime validation in signin

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import Email from "next-auth/providers/email";
 import { z } from "zod";
 import SigninSchema from "~/schemas/Signin.schema";
 
@@ -13,14 +12,20 @@ const form = reactive({
 });
 const errors = reactive({
   e: false,
-  email: "",
-  password: "",
+  email: {
+    message: "",
+    show: false,
+  },
+  password: {
+    message: "",
+    show: false,
+  },
 });
 
 const handleSubmit = async () => {
   try {
-    errors.email = ""; // Reset errors
-    errors.password = ""; // Reset errors
+    errors.email.message = ""; // Reset errors
+    errors.password.message = ""; // Reset errors
     await SigninSchema.parseAsync(form); // Validasi
     // Jika validasi berhasil, lakukan proses pendaftaran
     console.log("Form data is valid!", form);
@@ -28,11 +33,14 @@ const handleSubmit = async () => {
     if (error instanceof z.ZodError) {
       error.errors.forEach((err: any) => {
         if (err.path[0] == "email") {
-          errors.email = err.message;
+          errors.email.message = err.message;
+          errors.email.show = true;
         }
         if (err.path[0] == "password") {
-          errors.password = err.message;
+          errors.password.message = err.message;
+          errors.password.show = true;
         }
+        errors.e = true;
         // masih sapai disini dan untuk realtime validasi juga belum.
       });
     }
@@ -42,10 +50,30 @@ const handleSubmit = async () => {
 watch(form, async () => {
   try {
     await SigninSchema.parseAsync(form);
+    errors.e = false; // Reset error
+    errors.email.show = false; // Reset error
+    errors.password.show = false; // Reset error
   } catch (e: any) {
     e.errors.forEach((err: any) => {
-      console.log(err);
       // errors.value[err.path[0]] = err.message; // Set error message
+      switch (err.path[0]) {
+        case "email":
+          errors.email.message = err.message;
+          errors.email.show = true;
+          if (e.errors.length < 2) {
+            errors.password.show = false;
+          }
+          break;
+        case "password":
+          errors.password.message = err.message;
+          errors.password.show = true;
+          if (e.errors.length < 2) {
+            errors.email.show = false;
+          }
+          break;
+        default:
+          break;
+      }
     });
   }
 });
@@ -59,14 +87,18 @@ watch(form, async () => {
     <el-form :model="form" label-width="auto" @submit.prevent style="max-width: 600px">
       <el-form-item label="email" label-position="top" class="w-full">
         <el-input v-model="form.email" />
-        <div v-if="true" class="text-red-500">{{ errors.email }}</div>
+        <div v-if="errors.email.show" class="text-red-500">
+          {{ errors.email.message }}
+        </div>
       </el-form-item>
       <el-form-item label="password" label-position="top">
         <el-input type="password" placeholder="********" v-model="form.password" />
-        <div v-if="true" class="text-red-500">{{ errors.password }}</div>
+        <div v-if="errors.password.show" class="text-red-500">
+          {{ errors.password.message }}
+        </div>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="handleSubmit" :loading="false" :disabled="false"
+        <el-button type="primary" @click="handleSubmit" :disabled="errors.e"
           >Login</el-button
         >
       </el-form-item>
