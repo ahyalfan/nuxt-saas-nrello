daf4b0d212ecdcc91d40642bae100421a4e3c3c5
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-17 16:51:26 +0700

    feat: darkmode switcher

---
+++ components/ColorSwitcher.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Sunny, Moon } from "@element-plus/icons-vue";
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+</script>
+
+<template>
+  <!-- <ClientOnly> -->
+  <!-- <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray" variant="ghost"
+      aria-label="Theme" @click="isDark = !isDark" /> -->
+  <el-button color="white" size="large" :icon="isDark ? Moon : Sunny" />
+
+  <!-- <template #fallback>
+      <div class="w-8 h-8" />
+    </template>
+  </ClientOnly> -->
+</template>
