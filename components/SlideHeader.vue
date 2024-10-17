1c03c0a14d53bdadd4ebe90f6e8d5dc46684d141
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-17 16:51:49 +0700

    feat: slide header

---
+++ components/SlideHeader.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  onClick: () => void;
+}>();
+</script>
+<template>
+  <div class="flex dark:border-gray-800">
+    <h3 class="font-semibold">{{ title }}</h3>
+    <el-button @click="onClick">
+      <!-- <el-icon>
+        <Close />
+      </el-icon> -->
+    </el-button>
+  </div>
+</template>
+
+<style></style>
