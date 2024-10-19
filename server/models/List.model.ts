5428909585d2a08c7c53eb65cad4a24325961a0e
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-19 12:24:26 +0700

    create: list model

---
+++ server/models/List.model.ts
@@ -0,0 +1,42 @@
+import { defineMongooseModel } from '#nuxt/mongoose';
+import { Schema, model } from "mongoose";
+import type { Document } from "mongoose";
+
+export interface ListDocument extends Document {
+ name: string;
+  cards: string[];
+  board: string;
+  owner: string;
+}
+
+const listSchema = defineMongooseModel<ListDocument>({
+    name: 'List',
+    schema:{
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+        },
+        cards: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Card",
+            },
+        ],
+        board: {
+            type: Schema.Types.ObjectId as any,
+            ref: "Board",
+        },
+        owner: {
+            type: Schema.Types.ObjectId as any,
+            ref: "User",
+        },
+    },
+    options: {
+        timestamps: true,
+    },
+    hooks(schema) {
+        
+    }
+});
+
+export const List = listSchema;
