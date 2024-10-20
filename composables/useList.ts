23f3168a735990a0db6ca7a551d885305a8c8afd
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-20 13:48:27 +0700

    feat:composable for list

---
+++ composables/useList.ts
@@ -0,0 +1,38 @@
+// composable ini berbeda dengan store, karena ini hanya akan cersifat local
+// jadi gini misal useList ini di gunakan di component a dan b, ketika a meenggunakan useList maka useList di component b
+// tidak berpengaruh apapun, nah kalau store bisa berpengaruh karena store nilanya global
+export const useList = () => {
+  async function destroy(id: string, onDestroy?: () => void) {
+    try {
+      async () => {
+        await useFetch(`/api/lists/${id}`, {
+          method: "DELETE",
+        });
+        onDestroy?.();
+      }
+
+    } catch (e: any) {
+      ElNotification({
+        title: "Error",
+        message: e.message || "Something went wrong",
+        type: "error",
+        position: "top-left",
+      });
+    }
+  }
+
+  
+  async function update(id: string, data: Record<string, unknown>) {
+    await useFetch(`/api/lists/${id}`, {
+      body: data,
+      method: "PUT",
+      watch: false,
+    });
+  }
+
+  return {
+    destroy,
+    update,
+  };
+
+}
