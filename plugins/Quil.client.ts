fde78d70a33d1c258ba08e7270d3e881e8404f67
Ahmad Yogi Al fandi <138632356+ahyalfan@users.noreply.github.com>
2025-04-19 13:28:32 +0700

    Update Quil.client.ts

--- plugins/Quil.client.ts
+++ plugins/Quil.client.ts
@@ -4,5 +4,7 @@ import "@vueup/vue-quill/dist/vue-quill.snow.css";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("QuillEditor", QuillEditor);
 });
+// ini tidak sekedar vue app.componnte saja tapi bisa lain lain pokoknya yang ada di vue app dari nuxt
+// dan bisa lain lain tidak vue app saja misal provide hook dll
 
 // jadi plugin ini untuk menabhkan libary yg tidak terintegrasikan dengan nuxt js, yang mana kita perlu mendaftarkanya terlebih dahulu
